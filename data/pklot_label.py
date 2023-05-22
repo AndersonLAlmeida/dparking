4874544bfa693cac7e918c3a61548d1976787f70
AndersonLAlmeida <andersonalmeida98@hotmail.com>
2023-05-22 01:06:43 -0300

    Adicionando arquivos de treino e teste

--- data/pklot_label.py
+++ data/pklot_label.py
@@ -6,6 +6,8 @@ from os.path import join
 from PIL import Image
 import numpy as np
 
+sets=[('PUCPR', '2699'), ('UFPR04', '2232'), ('UFPR05', '2521')]
+
 
 def get_parent_dir(n=1):
     """returns the n-th parent dicrectory of the current
@@ -100,6 +102,9 @@ def convert_annotation(source_xml, label_dir):
                         xmax = point.attrib.get("x")
                
         cls_id = spaces.attrib.get("occupied")
+        if cls_id is None:
+            print("Descartando uma vaga por nao conter a classificacao")
+            continue
         b = (float(xmin), float(xmax), float(ymin), float(ymax))
         bb = convert((w,h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
@@ -109,10 +114,11 @@ def convert_annotation(source_xml, label_dir):
         xmin = -1
         xmax = -1
 
-def labels_from_xml(directory, path_name="", label_dir=""):
+def labels_from_xml(directory, path_name="", label_dir="", train_value=0, train_test_path=os.path.join(get_parent_dir(0))):
     # First get all images and xml files from path and its subfolders
     image_paths = GetFileList(directory, ".jpg")
     xml_paths = GetFileList(directory, ".xml")
+    count = 0
     
     if not len(image_paths) == len(xml_paths):
         print("number of annotations doesnt match number of images")
@@ -125,21 +131,42 @@ def labels_from_xml(directory, path_name="", label_dir=""):
         source_xml = image.replace(".jpg", ".xml")
         
         convert_annotation(source_xml, label_dir)
+        
+        if count <= train_value:
+            train_file = open(train_test_path + "/train.txt", "a+")
+            train_file.write(image)
+            train_file.write("\n")
+            train_file.close()
+        elif count > train_value:
+            test_file = open(train_test_path + "/test.txt", "a+")
+            test_file.write(image)
+            test_file.write("\n")
+            test_file.close()
+        count = count + 1
 
 
 if __name__ == "__main__":
     PUCPR_path = os.path.join(get_parent_dir(0), "PKLot", "PUCPR")
     UFPR04_path = os.path.join(get_parent_dir(0), "PKLot", "UFPR04")
     UFPR05_path = os.path.join(get_parent_dir(0), "PKLot", "UFPR05")
+    
+    PKLot_path = os.path.join(get_parent_dir(0), "PKLot")
 
     dirs_paths = [PUCPR_path, UFPR04_path, UFPR05_path]
+    i = 0
     for dir_path in dirs_paths:
+        dataset, train_value = sets[i]
         for diretorio, subpastas, arquivos in os.walk(dir_path):
             for subpasta in subpastas:
                 for dir, subs, arq in os.walk(diretorio +"/"+subpasta):
                     for sub in subs:
                         for d, s, a in os.walk(dir+"/"+sub):
-                            labels_from_xml(d, path_name=d, label_dir=dir_path)
+                            labels_from_xml(d, path_name=d, label_dir=dir_path, train_value=int(train_value), train_test_path=PKLot_path)
+                            
+        i = i + 1
+        
+    
+        
     
     
     
